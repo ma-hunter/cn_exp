d8b277930328906b1bfba5246ae2d180ee457211
Shiroha <i@shiraha.cn>
2021-06-10 18:39:56 +0800

    Add test executable.

---
+++ test/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Shiroha on 21/06/10.
+//
+#include <iostream>
+
+#include "definitions.h"
+#include "utils.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+
+    defs::ipv6_addr addr{{12, 45, 219, 78, 87, 0, 43, 119}};
+
+    std::cout << addr << ' ' << to_hex_string(100) << std::endl;
+
+    return 0;
+}
