c1a1b7bafe5e115ac4ed87f3c65ee4ecff895d56
Shiroha <i@shiraha.cn>
2021-06-10 18:58:31 +0800

    Add template for write address.

--- include/utils.h
+++ include/utils.h
@@ -10,27 +10,59 @@
 
 #include "definitions.h"
 
-#define NUMERIC_INTEGRAL_CHECK \
+#define NUMERIC_INTEGRAL_CHECK(integral) \
     static_assert(numeric_limits<integral>::is_integer, "Integer is required.")
-#define TYPE_TRAITS_INTEGRAL_CHECK \
+#define TYPE_TRAITS_INTEGRAL_CHECK(integral) \
     static_assert(is_integral<integral>::value, "Integral is required.")
+#define OUTPUT_ALLOW_RADIX_CHECK(radix) \
+    static_assert(radix == 10 || radix == 16, "Radix is not allowed.")
 
 namespace utils {
     using namespace std;
     using namespace defs;
 
     template <typename integral> string to_hex_string(integral x) {
-        NUMERIC_INTEGRAL_CHECK;
-        TYPE_TRAITS_INTEGRAL_CHECK;
+        NUMERIC_INTEGRAL_CHECK(integral);
+        TYPE_TRAITS_INTEGRAL_CHECK(integral);
         static char buf[20];
         itoa(x, buf, 16);
         return move(string(buf));
     }
+
+    template <typename integral>
+    inline const char *cover_to_hex(integral x) {
+        TYPE_TRAITS_INTEGRAL_CHECK(integral);
+        static char buf[24];
+        fill(buf, buf + 20, '0');
+        itoa(x, buf + sizeof(integral), 16);
+        return buf + strlen(buf + sizeof(integral));
+    }
+
     string to_hex_string(short x);
+
+    template <typename integral, unsigned radix, bool cover>
+    ostream &write_addr(ostream &os, const integral *st, const integral *ed, char split) {
+        NUMERIC_INTEGRAL_CHECK(integral);
+        TYPE_TRAITS_INTEGRAL_CHECK(integral);
+        OUTPUT_ALLOW_RADIX_CHECK(radix);
+        for (auto cur = st; cur != ed; ++ cur) {
+            if (cur != st) os << split;
+            if constexpr (!cover) {
+                if constexpr (radix == 16)
+                    os << to_hex_string(*cur);
+                else os << to_string(*cur);
+            } else if constexpr (radix == 16)
+                os << cover_to_hex(*cur);
+            else os << *cur;
+        }
+        return os;
+    }
 
     ostream &operator <<(ostream &os, const ipv4_addr &ip);
 
     ostream &operator <<(ostream &os, const ipv6_addr &ip);
+
+    ostream &operator <<(ostream &os, const mac_addr &ip);
 }
 using namespace utils;
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -6,15 +6,6 @@
 using namespace std;
 using namespace defs;
 
-template <typename integral>
-inline const char *int_to_hex(integral x) {
-    TYPE_TRAITS_INTEGRAL_CHECK;
-    static char buf[24];
-    fill(buf, buf + 20, '0');
-    itoa(x, buf + sizeof(integral), 16);
-    return buf + strlen(buf + sizeof(integral));
-}
-
 ostream &utils::operator <<(ostream &os, const ipv4_addr &ip) {
     int cnt = 4;
     for (auto byte : ip.bt) {
@@ -27,7 +18,16 @@ ostream &utils::operator <<(ostream &os, const ipv4_addr &ip) {
 ostream &utils::operator <<(ostream &os, const ipv6_addr &ip) {
     int cnt = 8;
     for (auto part : ip.pt) {
-        os << int_to_hex(part);
+        os << cover_to_hex(part);
+        if (-- cnt) os << ':';
+    }
+    return os;
+}
+
+ostream &utils::operator<<(ostream &os, const mac_addr &ip) {
+    int cnt = 6;
+    for (auto byte : ip.bt) {
+        os << cover_to_hex(byte);
         if (-- cnt) os << ':';
     }
     return os;
